d62ba7d608c05895eedd7cae98f2b2a8b8418bb8
UtkarshMish <iiutkarshmishraii@gmail.com>
2020-03-29 16:55:06 +0530

    added video saturation

---
+++ static/react/precache-manifest.3ea2ad35cc454856312ae4648024a375.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a67f1dc22f60edd9b8b9c9217c587f",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "1a6de306498acf989df7",
+    "url": "/static/react/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "c5aca6599adc6bd8d0ec",
+    "url": "/static/react/css/main.c0a4b038.chunk.css"
+  },
+  {
+    "revision": "1a6de306498acf989df7",
+    "url": "/static/react/js/2.48d5296b.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/react/js/2.48d5296b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5aca6599adc6bd8d0ec",
+    "url": "/static/react/js/main.213fc475.chunk.js"
+  },
+  {
+    "revision": "d9daf1b770682de9343a",
+    "url": "/static/react/js/runtime-main.9e6d6922.js"
+  },
+  {
+    "revision": "9f09eedc672ebe00a6b6ef17c1b95b7d",
+    "url": "/static/react/media/video.9f09eedc.mp4"
+  }
+]);
