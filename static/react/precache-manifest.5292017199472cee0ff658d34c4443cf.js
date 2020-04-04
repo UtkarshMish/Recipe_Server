df232f170424e37bde9770177eb6e03b0964973a
UtkarshMish <iiutkarshmishraii@gmail.com>
2020-04-05 00:37:35 +0530

    updated design

---
+++ static/react/precache-manifest.5292017199472cee0ff658d34c4443cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24a3653fe3ef054a5edbb8753251363",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "9202514489497b393d6a",
+    "url": "/static/react/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "a252545ca7f670fac650",
+    "url": "/static/react/css/main.be46a6da.chunk.css"
+  },
+  {
+    "revision": "9202514489497b393d6a",
+    "url": "/static/react/js/2.fee52b34.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/react/js/2.fee52b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a252545ca7f670fac650",
+    "url": "/static/react/js/main.cc7d4133.chunk.js"
+  },
+  {
+    "revision": "d9daf1b770682de9343a",
+    "url": "/static/react/js/runtime-main.9e6d6922.js"
+  },
+  {
+    "revision": "9f09eedc672ebe00a6b6ef17c1b95b7d",
+    "url": "/static/react/media/video.9f09eedc.mp4"
+  }
+]);
