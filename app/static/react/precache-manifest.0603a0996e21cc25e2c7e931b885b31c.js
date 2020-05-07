b38ec3b84200de7f862db83f8bef1f30b7bcb1f9
UtkarshMish <iiutkarshmishraii@gmail.com>
2020-05-07 20:30:51 +0530

    remove console logs

---
+++ app/static/react/precache-manifest.0603a0996e21cc25e2c7e931b885b31c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2478afc131ec501771261bf1e9f74d5d",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "41a428d8e1268cc39ed9",
+    "url": "/static/react/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "8955ab59f26c1c0478ff",
+    "url": "/static/react/css/main.85088b98.chunk.css"
+  },
+  {
+    "revision": "41a428d8e1268cc39ed9",
+    "url": "/static/react/js/2.4f74141e.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/react/js/2.4f74141e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8955ab59f26c1c0478ff",
+    "url": "/static/react/js/main.bb84450e.chunk.js"
+  },
+  {
+    "revision": "d9daf1b770682de9343a",
+    "url": "/static/react/js/runtime-main.9e6d6922.js"
+  },
+  {
+    "revision": "9f09eedc672ebe00a6b6ef17c1b95b7d",
+    "url": "/static/react/media/video.9f09eedc.mp4"
+  }
+]);
