e06a26a17f5093b7a09c79494889c90cf0b730c2
UtkarshMish <iiutkarshmishraii@gmail.com>
2020-05-08 13:57:07 +0530

    updated structure

--- static/react/precache-manifest.c8cc662e3a373739a4543dbbc2b6da25.js
+++ static/react/precache-manifest.c8cc662e3a373739a4543dbbc2b6da25.js
@@ -1,16 +1,16 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f41136330ecf16db37eef02259e638f5",
-    "url": "/static/react/../../templates/index.html"
+      "revision": "eab7fcdd98b64eedbb63f5bbb93d42bd",
+      "url": "/static/react/../../templates/index.html"
   },
   {
     "revision": "41a428d8e1268cc39ed9",
     "url": "/static/react/css/2.5dbdccff.chunk.css"
   },
-  {
-    "revision": "88f9f35734aced68a2d9",
-    "url": "/static/react/css/main.b8a8ab84.chunk.css"
-  },
+    {
+        "revision": "905a631e6ba276085241",
+        "url": "/static/react/css/main.00bf6c65.chunk.css"
+    },
   {
     "revision": "41a428d8e1268cc39ed9",
     "url": "/static/react/js/2.4f74141e.chunk.js"
@@ -19,10 +19,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "revision": "56729d980da09961afe8d497a21eec12",
     "url": "/static/react/js/2.4f74141e.chunk.js.LICENSE.txt"
   },
-  {
-    "revision": "88f9f35734aced68a2d9",
-    "url": "/static/react/js/main.4bc4c9f0.chunk.js"
-  },
+    {
+        "revision": "905a631e6ba276085241",
+        "url": "/static/react/js/main.2a439864.chunk.js"
+    },
   {
     "revision": "d9daf1b770682de9343a",
     "url": "/static/react/js/runtime-main.9e6d6922.js"
