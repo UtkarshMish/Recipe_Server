7ebd9eacafc5f32e1c08cabd2d3113e9fc624482
UtkarshMish <iiutkarshmishraii@gmail.com>
2020-05-14 08:14:57 +0530

    fixed minor bug in recommender

--- Recipe_Model.py
+++ Recipe_Model.py
@@ -18,7 +18,7 @@ def hybrid_recommender(model, data, recipe_ids):
     for reci_id in recipe_ids:
         scores.append(model.predict(reci_id, np.arange(data), num_threads=4))
     scores = np.array([max(i) for i in zip(*scores)])
-    return [i for i in np.argsort(-scores)[:-5:-1] if i not in recipe_ids]
+    return [i for i in np.argsort(-scores) if i not in recipe_ids][:-5:-1]
 
 
 def vectorizer(matrix):
