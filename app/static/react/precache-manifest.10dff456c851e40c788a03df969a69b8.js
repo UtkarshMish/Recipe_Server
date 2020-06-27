a5392e35a55a5eb5350809d14474ae3f19b30f28
UtkarshMish <iiutkarshmishraii@gmail.com>
2020-06-27 17:52:33 +0530

    fixed adsense

---
+++ app/static/react/precache-manifest.10dff456c851e40c788a03df969a69b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d612196ed319a24459786b72bde8ee2",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "91a578126cd251dcfc3e",
+    "url": "/static/react/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "5176d5b43cf03b6c0de5",
+    "url": "/static/react/css/main.61ddeb6e.chunk.css"
+  },
+  {
+    "revision": "91a578126cd251dcfc3e",
+    "url": "/static/react/js/2.2dafd490.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/react/js/2.2dafd490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5176d5b43cf03b6c0de5",
+    "url": "/static/react/js/main.8b568ec7.chunk.js"
+  },
+  {
+    "revision": "d9daf1b770682de9343a",
+    "url": "/static/react/js/runtime-main.9e6d6922.js"
+  },
+  {
+    "revision": "84ffe68febe8c934a7c628287cce233b",
+    "url": "/static/react/media/office.84ffe68f.jpeg"
+  },
+  {
+    "revision": "9f09eedc672ebe00a6b6ef17c1b95b7d",
+    "url": "/static/react/media/video.9f09eedc.mp4"
+  }
+]);
--- app/static/react/service-worker.js
+++ app/static/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/static/react/precache-manifest.3af6dcee2516fc0a95c9b1254bf348ff.js"
+  "/static/react/precache-manifest.10dff456c851e40c788a03df969a69b8.js"
 );
 
 self.addEventListener('message', (event) => {
